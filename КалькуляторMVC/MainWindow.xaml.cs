38e27ebdf10d626a9fbb85d1597f3456c8cf8d80
Dmitry <dima16082000@gmail.com>
2022-02-06 18:00:25 +0300

    init

---
+++ КалькуляторMVC/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Калькулятор
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.firstNumber = TBOXFirstNumber;
+            Model.secondNumber = TBOXSecondNumber;
+            Model.rav = TBLOCKRez;
+            Model.sign = TBLOCKSign;
+            CBOperation.ItemsSource = Model.SignList;
+        }
+
+        private void CBOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox CB = (ComboBox) sender;
+            Model.SignChange = CB.SelectedIndex;
+        }
+
+        private void BCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            Model.RAV = Model.firstN;
+            Model.RAV = Model.secondN;
+            TBLOCKRez.Text = Model.RAV + "";            
+        }
+    }
+}
---
+++ КалькуляторMVC/Model.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Калькулятор
+{
+    static class Model
+    {
+        public static List<string> SignList = new List<string>() { "Сложение", "Вычитание", "Умножение", "Деление"};
+
+        public static TextBox firstNumber, secondNumber;
+        public static TextBlock rav, sign;
+
+        static int SignNumber, RavNumber, IsFirst = 0;
+        public static int SignChange
+        {
+            get 
+            {       
+                return SignNumber;
+            }
+            set 
+            {
+                SignNumber = value;
+                switch (SignNumber)
+                {
+                    case 0://+
+                        sign.Text = "+";
+                        break;
+                    case 1://-
+                        sign.Text = "-";
+                        break;
+                    case 2://*
+                        sign.Text = "*";
+                        break;
+                    case 3://\
+                        sign.Text = "/";
+                        break;
+                }
+            }
+        }
+
+        public static int firstN
+        {
+            get 
+            {
+                return Convert.ToInt32(firstNumber.Text);
+            }
+        }
+
+        public static int secondN
+        {
+            get
+            {
+                return Convert.ToInt32(secondNumber.Text);
+            }
+        }
+
+        public static int RAV
+        {
+            get
+            {
+                int opa = RavNumber;
+                RavNumber = 0;
+                return opa;                
+            }
+            set
+            {
+                if (IsFirst == 0)
+                {
+                    RavNumber = value;
+                    IsFirst = 1;
+                }
+                else
+                {
+                    switch (SignNumber)
+                    {
+                        case 0://+
+                            RavNumber += value;
+                            break;
+                        case 1://-
+                            RavNumber -= value;
+                            break;
+                        case 2://*
+                            RavNumber *= value;
+                            break;
+                        case 3://\
+                            RavNumber /= value;
+                            break;
+                    }
+                    IsFirst = 0;
+                }
+            }
+        }
+    }
+}
